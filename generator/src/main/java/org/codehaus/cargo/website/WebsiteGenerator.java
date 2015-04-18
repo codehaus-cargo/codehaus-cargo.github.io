a758f466b6c19d49d12f9c8fbec146f914d90a56
Savas Ali Tokmen <nospam@alishomepage.com>
2015-04-18 17:53:06 +0200

    Created first version of the Codehaus Cargo website

---
+++ generator/src/main/java/org/codehaus/cargo/website/WebsiteGenerator.java
@@ -0,0 +1,350 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2011 Vincent Massol, 2011-2015 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.website;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WebsiteGenerator implements Runnable
+{
+    private static Set<File> files;
+
+    private static Set<URL> attachments;
+
+    private static Map<URL, Exception> exceptions;
+
+    private static final boolean downloadAttachments = true;
+
+    private static final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(8);
+
+    public static void main(String[] args) throws Exception
+    {
+        download();
+        parse();
+    }
+
+    private static void download() throws Exception
+    {
+        long start = System.currentTimeMillis();
+
+        File attachmentsDirectory = new File("target", "attachments");
+        if (!attachmentsDirectory.isDirectory())
+        {
+            attachmentsDirectory.mkdirs();
+        }
+        File sourceDirectory = new File("target", "source");
+        if (!sourceDirectory.isDirectory())
+        {
+            sourceDirectory.mkdirs();
+        }
+        File tempDirectory = new File("target", "temp");
+        if (!tempDirectory.isDirectory())
+        {
+            tempDirectory.mkdirs();
+        }
+
+        URL url = new URL("https://codehaus-cargo.atlassian.net/wiki/rest/api/space/CARGO/content/page?limit=2048");
+        URLConnection connection = url.openConnection();
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream())))
+        {
+            for (String line = reader.readLine(); line != null; line = reader.readLine())
+            {
+                sb.append(line);
+            }
+        }
+        JSONArray results = new JSONObject(sb.toString()).getJSONArray("results");
+        System.out.println("Found " + results.length() + " objects to handle");
+
+        files = Collections.synchronizedSet(new HashSet<File>(results.length()));
+        exceptions = Collections.synchronizedMap(new HashMap<URL, Exception>());
+        attachments = Collections.synchronizedSet(new HashSet<URL>(results.length()));
+        for (int i = 0; i < results.length(); i++)
+        {
+            JSONObject links = results.getJSONObject(i).getJSONObject("_links");
+            ConfluenceTest runnable = new ConfluenceTest();
+            runnable.url = new URL(links.getString("self") + "?expand=body.view");
+            Thread thread = new Thread(runnable);
+            executor.submit(thread);
+        }
+
+        while (executor.getCompletedTaskCount() < results.length() + attachments.size())
+        {
+            Thread.sleep(5000);
+            System.out.println("  - Completed " + executor.getCompletedTaskCount() + "/"
+                + (results.length() + attachments.size()) + " tasks - "
+                +  ((System.currentTimeMillis() - start) / 1000) + " seconds spent so far");
+        }
+        if (executor.getCompletedTaskCount() < results.length() + attachments.size())
+        {
+            throw new Exception("WARNING: Only completed " + executor.getCompletedTaskCount() + " tasks out of " + (results.length() + attachments.size()));
+        }
+        System.out.println("All tasks complete");
+        for (File file : files)
+        {
+            System.out.println("  - Wrote file " + file.getAbsolutePath());
+        }
+        if (exceptions.size() > 0)
+        {
+            for (HashMap.Entry<URL, Exception> exception : exceptions.entrySet())
+            {
+                System.out.println("  - Pending exception for URL " + exception.getKey() + ": " + exception);
+            }
+            throw new Exception("Some files have failed download");
+        }
+        System.out.println("Export completed, total time taken " + ((System.currentTimeMillis() - start) / 1000) + " seconds");
+    }
+
+    private static void parse() throws Exception
+    {
+        System.out.println("Parsing files and generating Web site");
+        File target = new File("target");
+        File classes = new File(target, "classes");
+        Files.copy(new File(classes, "rss.gif").toPath(),
+            new File(target, "attachments/rss.gif").toPath(), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(new File(classes, "favicon.ico").toPath(),
+            new File(target, "attachments/favicon.ico").toPath(), StandardCopyOption.REPLACE_EXISTING);
+        writeFile(new File(target, "site.css"), readFile(new File(classes, "site.css")));
+        File sourceDirectory = new File(target, "source");
+        String template = readFile(new File(target, "classes/cargo-template.html"));
+        String navigation = readFile(new File(sourceDirectory, "Navigation"));
+        template = template.replace("$navigation", navigation);
+        for (File sourceFile : sourceDirectory.listFiles())
+        {
+            String name = sourceFile.getName();
+            File file = new File(target, name + ".html");
+            String value = readFile(sourceFile);
+            value = value.replaceAll(
+                "<script type=\"syntaxhighlighter\"[^>]+><\\!\\[CDATA\\[", "<pre>");
+            value = value.replace("]]></script>", "</pre>");
+            writeFile(file, template.replace("$name", name).replace("$title",
+                URLDecoder.decode(name, "UTF-8")).replace("$value", value));
+            System.out.println("  - Wrote file " + file.getAbsolutePath());
+        }
+        System.out.println("Parsing complete");
+    }
+
+    private static String readFile(File f) throws IOException
+    {
+        byte[] bytes = Files.readAllBytes(f.toPath());
+        return new String(bytes, Charset.forName("UTF-8"));
+    }
+
+    private static void writeFile(File f, String value) throws IOException
+    {
+        try (PrintWriter writer = new PrintWriter(f, "UTF-8"))
+        {
+            writer.write(value.replace("\r\n", "\n").replace("\n\r", "\n").replace("\r", "\n").replace("\n", "\r\n"));
+        }
+    }
+
+    private URL url;
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            String value = "";
+            for (int i = 0; i < 10; i++)
+            {
+                URLConnection connection = url.openConnection();
+                try (InputStream is = connection.getInputStream())
+                {
+                    String filePath = url.getPath();
+                    int questionMark = filePath.lastIndexOf('?');
+                    if (questionMark != -1)
+                    {
+                        filePath = filePath.substring(filePath.lastIndexOf('/'), questionMark);
+                    }
+                    else
+                    {
+                        filePath = filePath.substring(filePath.lastIndexOf('/'));
+                    }
+                    File file = new File("target");
+                    if (url.getPath().contains("/wiki/rest/api/content/"))
+                    {
+                        file = new File(file, "temp");
+                    }
+                    else
+                    {
+                        file = new File(file, "attachments");
+                    }
+                    file = new File(file, filePath);
+                    try (FileOutputStream fos = new FileOutputStream(file))
+                    {
+                        byte[] buffer = new byte[8 * 1024];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1)
+                        {
+                            fos.write(buffer, 0, bytesRead);
+                        }
+                    }
+                    if (url.getPath().contains("/wiki/rest/api/content/"))
+                    {
+                        value = readFile(file);
+                    }
+                }
+                catch (IOException e)
+                {
+                    if (i == 9)
+                    {
+                        throw e;
+                    }
+                    Thread.sleep(1000);
+                }
+            }
+
+            if (!value.isEmpty())
+            {
+                JSONObject result = new JSONObject(value);
+                value = result.getJSONObject("body").getJSONObject("view").getString("value");
+
+                Pattern pattern = Pattern.compile("href=\"/wiki/display/CARGO/[^\"]+\"|href='/wiki/display/CARGO/[^']+'");
+                Matcher matcher = pattern.matcher(value);
+                int start = 0;
+                StringBuilder sb = new StringBuilder();
+                while (matcher.find())
+                {
+                    sb.append(value.substring(start, matcher.start()));
+                    sb.append("href=\"");
+                    sb.append(value.substring(matcher.start() + 26, matcher.end() - 1));
+                    sb.append(".html\"");
+                    start = matcher.end();
+                }
+                sb.append(value.substring(start));
+                value = sb.toString();
+
+                pattern = Pattern.compile("href=\"/wiki/download/attachments/[^\"]+\"|href='/wiki/download/attachments/[^']+'");
+                matcher = pattern.matcher(value);
+                start = 0;
+                sb = new StringBuilder();
+                while (matcher.find())
+                {
+                    sb.append(value.substring(start, matcher.start()));
+                    sb.append("href=\"attachments");
+                    String attachment = value.substring(matcher.start() + 6, matcher.end() - 1);
+                    if (attachment.startsWith("/"))
+                    {
+                        attachment = "https://codehaus-cargo.atlassian.net" + attachment;
+                    }
+                    if (downloadAttachments)
+                    {
+                        URL attachmentUrl = new URL(attachment);
+                        synchronized (attachments)
+                        {
+                            if (!attachments.contains(attachmentUrl))
+                            {
+                                attachments.add(attachmentUrl);
+                                ConfluenceTest runnable = new ConfluenceTest();
+                                runnable.url = attachmentUrl;
+                                Thread thread = new Thread(runnable);
+                                executor.submit(thread);
+                            }
+                        }
+                    }
+                    int questionMark = attachment.lastIndexOf('?');
+                    if (questionMark != -1)
+                    {
+                        sb.append(attachment.substring(attachment.lastIndexOf('/'), questionMark));
+                    }
+                    else
+                    {
+                        sb.append(attachment.substring(attachment.lastIndexOf('/')));
+                    }
+                    sb.append("\"");
+                    start = matcher.end();
+                }
+                sb.append(value.substring(start));
+                value = sb.toString();
+
+                pattern = Pattern.compile("src=\"[^\"]+\"|src='[^']+'");
+                matcher = pattern.matcher(value);
+                start = 0;
+                sb = new StringBuilder();
+                while (matcher.find())
+                {
+                    sb.append(value.substring(start, matcher.start()));
+                    sb.append("src=\"attachments/");
+                    String attachment = value.substring(matcher.start() + 5, matcher.end() - 1);
+                    if ("http://www.codehaus.org/newtest.gif".equals(attachment))
+                    {
+                        attachment = "blank.gif";
+                    }
+                    else if (attachment.startsWith("/"))
+                    {
+                        attachment = "https://codehaus-cargo.atlassian.net" + attachment;
+                    }
+                    if (!"blank.gif".equals(attachment))
+                    {
+                        URL attachmentUrl = new URL(attachment);
+                        synchronized (attachments)
+                        {
+                            if (!attachments.contains(attachmentUrl))
+                            {
+                                attachments.add(attachmentUrl);
+                                ConfluenceTest runnable = new ConfluenceTest();
+                                runnable.url = attachmentUrl;
+                                Thread thread = new Thread(runnable);
+                                executor.submit(thread);
+                            }
+                        }
+                    }
+                    sb.append(attachment.substring(attachment.lastIndexOf('/') + 1));
+                    sb.append("\"");
+                    start = matcher.end();
+                }
+                sb.append(value.substring(start));
+                value = sb.toString();
+
+                String title = result.getString("title");
+                File file = new File("target/source", URLEncoder.encode(title, "UTF-8"));
+                writeFile(file, value);
+                files.add(file);
+            }
+        }
+        catch (Exception e)
+        {
+            exceptions.put(url, e);
+        }
+    }
+}
---
+++ generator/src/test/java/org/codehaus/cargo/website/WebsiteGeneratorTest.java
@@ -0,0 +1,31 @@
+/*
+ * ========================================================================
+ *
+ * Codehaus CARGO, copyright 2004-2011 Vincent Massol, 2011-2015 Ali Tokmen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+package org.codehaus.cargo.website;
+
+import org.junit.Test;
+
+public class WebsiteGeneratorTest
+{
+    @Test
+    public void generateWebsite() throws Exception
+    {
+        WebsiteGenerator.main(new String[0]);
+    }
+}
